8f4a802229ba77b247f78554bf62b110fc9d5869
NguyenThiLuong <luongnhi0209@gmail.com>
2019-04-04 22:48:04 +0700

    bai 12

--- Unit11/Bai-11-2/js/script.js
+++ Unit11/Bai-11-2/js/script.js
@@ -1,7 +1,11 @@
 $(function(){
 	$('button').click(function(){
 		var check = $(this).html();
-		if (check == '<span class="fa fa-plus fa-2x"></span>') {
+		$('.content').css('display','none');
+		$('button').html('<span class="fa fa-plus fa-2x"></span>');
+		$('.text').css('color','white');
+		if (check == '<span class="fa fa-plus fa-2x"></span>'){
+			$('')
 			$(this).html('<span class="fa fa-minus fa-2x"></span>');
 			$(this).parent().next().show(500);
 			$(this).next().css('color','#BDE453');
